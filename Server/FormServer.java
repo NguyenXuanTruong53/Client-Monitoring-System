932840c96cb1719c716d05e261acd6b224dd1402
Nguyễn Xuân Trường <truongnx@minerva.vn>
2022-08-27 11:30:47 +0700

    - fix connect server to client

--- Server/FormServer.java
+++ Server/FormServer.java
@@ -9,19 +9,25 @@ import javax.swing.border.EmptyBorder;
 import javax.swing.JLabel;
 import javax.swing.JButton;
 import javax.swing.JTable;
+import javax.swing.table.DefaultTableModel;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
 
 public class FormServer extends JFrame {
 
 	private JPanel contentPane;
 	private JTable table;
-
+	private DefaultTableModel model = new DefaultTableModel();
+	private JTable table_server = new JTable(model);
 	/**
 	 * Launch the application.
 	 */
 	public static void main(String[] args) {
-		EventQueue.invokeLater(new Runnable() {
+				EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
 					FormServer frame = new FormServer();
@@ -65,16 +71,80 @@ public class FormServer extends JFrame {
 		contentPane.add(lblNewLabel_3_1);
 
 		JButton btnKetNoi = new JButton("Kết nối");
+//		run_server1();
 		btnKetNoi.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				MultithreadedSocketServer.run_server();
+				run_server1();
 			}
 		});
 		btnKetNoi.setBounds(44, 102, 89, 23);
 		contentPane.add(btnKetNoi);
-		
-		table = new JTable();
-		table.setBounds(24, 161, 388, 89);
-		contentPane.add(table);
+
+		model.addColumn("Col1");
+		model.addColumn("Col2");
+		model.addColumn("Col3");
+		model.addColumn("Col4");
+		model.addRow(new Object[]{"v1", "v2", "v3", "v4"});
+//		table_server = new JTable();
+		table_server.setBounds(24, 161, 388, 89);
+		contentPane.add(table_server);
+
+	}
+	public void run_server1(){
+
+		try{
+
+			ServerSocket server=new ServerSocket(8888);
+
+			int counter=0;
+			System.out.println("Server Started ....");
+			Socket serverClient=server.accept();  //server accept the client connection request
+			System.out.println(" >> " + "Client No:" + counter + " started!");
+			model.insertRow(1, new Object[]{1, "Client No:" + counter, "connect succecss", "ket noi thanh cong"});
+			ServerClientThread sct = new ServerClientThread(serverClient,counter); //send  the request to a separate thread
+			sct.start();
+//			while(true){
+//				counter++;
+//				Socket serverClient=server.accept();  //server accept the client connection request
+//				System.out.println(" >> " + "Client No:" + counter + " started!");
+//				model.insertRow(1, new Object[]{1, "Client No:" + counter, "connect succecss", "ket noi thanh cong"});
+//				ServerClientThread sct = new ServerClientThread(serverClient,counter); //send  the request to a separate thread
+//				sct.start();
+//			}
+		}catch(Exception e){
+			System.out.println(e);
+		}
 	}
 }
+class ServerClientThread extends Thread {
+	Socket serverClient;
+	int clientNo;
+	int squre;
+	ServerClientThread(Socket inSocket,int counter){
+		serverClient = inSocket;
+		clientNo=counter;
+	}
+	public void run(){
+		try{
+			DataInputStream inStream = new DataInputStream(serverClient.getInputStream());
+			DataOutputStream outStream = new DataOutputStream(serverClient.getOutputStream());
+			String clientMessage="", serverMessage="";
+			while(!clientMessage.equals("bye")){
+				clientMessage=inStream.readUTF();
+				System.out.println("From Client-" +clientNo+ ": Number is :"+clientMessage);
+				squre = Integer.parseInt(clientMessage) * Integer.parseInt(clientMessage);
+				serverMessage="From Server to Client-" + clientNo + " Square of " + clientMessage + " is " +squre;
+				outStream.writeUTF(serverMessage);
+				outStream.flush();
+			}
+			inStream.close();
+			outStream.close();
+			serverClient.close();
+		}catch(Exception ex){
+			System.out.println(ex);
+		}finally{
+			System.out.println("Client -" + clientNo + " exit!! ");
+		}
+	}
+}
+
